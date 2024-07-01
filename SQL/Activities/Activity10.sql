3026d75c080e46be8c3d0f704ae734187d715472
SaswatiChakraborty <Saswati.Chakraborty@ibm.com>
2024-07-01 23:42:43 +0530

    Add files via upload

--- SQL/Activities/Activity10.sql
+++ SQL/Activities/Activity10.sql
@@ -1,5 +1,5 @@
-REM   Script: Activity9
-REM   Join
+REM   Script: Activity10
+REM   Subquery operations
 
 create table customers ( 
     customer_id int primary key, customer_name varchar(32), 
@@ -71,4 +71,16 @@ INNER JOIN salesman b ON a.salesman_id = b.salesman_id WHERE commission > 12;
 
 SELECT order_no, order_date, purchase_amount, customer_name AS "Customer Name", grade, salesman_name AS "Salesman", commission FROM orders a  
 INNER JOIN customers b ON a.customer_id = b.customer_id INNER JOIN salesman c ON a.salesman_id = c.salesman_id;
+
+SELECT * FROM orders WHERE salesman_id = (SELECT DISTINCT salesman_id FROM orders WHERE customer_id = 3007);
+
+SELECT * FROM orders WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city = 'New York');
+
+SELECT * FROM orders WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE salesman_city = 'New York');
+
+SELECT grade, COUNT(*) FROM customers GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city = 'New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission = ( SELECT MAX(commission) FROM salesman));
 
--- SQL/Activities/Activity11.sql
+++ SQL/Activities/Activity11.sql
@@ -1,5 +1,5 @@
-REM   Script: Activity9
-REM   Join
+REM   Script: Activity11
+REM   Additional activity
 
 create table customers ( 
     customer_id int primary key, customer_name varchar(32), 
@@ -71,4 +71,42 @@ INNER JOIN salesman b ON a.salesman_id = b.salesman_id WHERE commission > 12;
 
 SELECT order_no, order_date, purchase_amount, customer_name AS "Customer Name", grade, salesman_name AS "Salesman", commission FROM orders a  
 INNER JOIN customers b ON a.customer_id = b.customer_id INNER JOIN salesman c ON a.salesman_id = c.salesman_id;
+
+SELECT * FROM orders WHERE salesman_id = (SELECT DISTINCT salesman_id FROM orders WHERE customer_id = 3007);
+
+SELECT * FROM orders WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city = 'New York');
+
+SELECT * FROM orders WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE salesman_city = 'New York');
+
+SELECT grade, COUNT(*) FROM customers GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city = 'New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission = ( SELECT MAX(commission) FROM salesman));
+
+SELECT customer_id, customer_name FROM customers a WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION SELECT salesman_id, name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT customer_id, customer_name FROM customers a WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id) 
+UNION SELECT salesman_id, salesman_name FROM salesman a 
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id) 
+ORDER BY customer_name;
+
+SELECT a.salesman_id, salesman_name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id = b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, salesman_name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+SELECT a.salesman_id, salesman_name, order_no, 'highest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id = b.salesman_id 
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date) 
+UNION 
+SELECT a.salesman_id, salesman_name, order_no, 'lowest on', order_date FROM salesman a, orders b 
+WHERE a.salesman_id=b.salesman_id 
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
 
